3de8f37efe81a5fd06806d643e3b7b73a66f6166
michelepiccinno <michele.piccinno@gmail.com>
2023-07-01 01:20:26 +0200

    add-double-price

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 let km = prompt('Quanti km devi percorrere?');
 let age = prompt('Quanti anni hai?');
-let fullPrice = km * 0.21;
+let fullPrice = (km * 0.21).toFixed(2);
 
 document.getElementById("my-km").innerHTML = km;
 document.getElementById("my-age").innerHTML = age;
 
 if (age < 18) {
   netPrice = (fullPrice * 0.8).toFixed(2);
-  document.getElementById("my-price").innerHTML = 'Hai un età inferiore ad anni 18 pertanto usufruirai di uno sconto del 20%. Il prezzo del tuo biglietto è di €: ' + netPrice;
+  document.getElementById("my-price").innerHTML = 'Hai un età inferiore ad anni 18 pertanto usufruirai di uno sconto del 20%. Il prezzo del tuo biglietto è di: <span class="text-decoration-line-through opacity-50"> € ' + fullPrice + '</span> € ' + netPrice;
 } else if (age > 65) {
   netPrice = (fullPrice * 0.6).toFixed(2);
-  document.getElementById("my-price").innerHTML = 'Hai un età maggiore di anni 65 pertanto usufruirai di uno sconto del 40%. Il prezzo del tuo biglietto è di €: ' + netPrice;
+  document.getElementById("my-price").innerHTML = 'Hai un età maggiore di anni 65 pertanto usufruirai di uno sconto del 40%. Il prezzo del tuo biglietto è di: <span class="text-decoration-line-through opacity-50"> € ' + fullPrice + '</span> € ' + netPrice;
 } else {
-document.getElementById("my-price").innerHTML = 'Il prezzo intero del biglietto è di € ' + (netPrice = fullPrice.toFixed(2));
+document.getElementById("my-price").innerHTML = 'Il prezzo intero del biglietto è di € ' + fullPrice;
 }
 
 //console.log('full price ' + fullPrice);
